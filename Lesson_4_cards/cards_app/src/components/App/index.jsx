755c5860081d96795125682c35aded2d4ef4fd70
Marina_Koch <107465368+MarinaKoch1985@users.noreply.github.com>
2022-11-19 18:33:23 +0100

    les_4

---
+++ Lesson_4_cards/cards_app/src/components/App/index.jsx
@@ -0,0 +1,61 @@
+import CardsContainer from "../CardsContainer";
+import { words } from '../../Data/words'
+import { useState } from "react"
+import React from "react";
+import Triggers from "../Triggers";
+import AddPostForn from "../AddPostForm";
+
+function App() {
+
+  const [ cards, setCards ] = useState(words);
+
+  const change_to_eng = () => {
+    setCards(cards.map(el => {
+      el.lang = 'eng';
+      return el
+    }))
+  }
+  const change_to_rus = () => {
+    setCards(cards.map(el => {
+      el.lang = 'rus';
+      return el
+     }))
+    
+  }
+
+  const change_lang = (id) => {
+    setCards(cards.map(el => {
+      if(el.id ===id){
+        el.lang = el.lang === 'rus' ? 'eng' : 'rus'
+      }
+      return el
+    }))
+
+  }
+
+  // const change_lang = (id) => {
+  //   cards[id - 1].lang = cards[id - 1].lang === 'rus' ? 'eng' : 'rus';
+  //   setCards([...cards]);
+  // }
+
+  const add_card = (rus_value, eng_value) => setCards ([
+    ...cards, 
+    {
+      id: Date.now(),// libo cards.length + 1
+      rus: rus_value,
+      eng: eng_value,
+      lang: 'eng'
+    }
+  ]);
+
+
+  return (
+    <div>
+      <AddPostForn add_card = {add_card}/>
+    <CardsContainer words_array = { cards } change_lang = {change_lang}/>
+    <Triggers change_to_eng={change_to_eng} change_to_rus={change_to_rus}/>
+    </div>
+  );
+}
+
+export default App;
