df7c0f4a2d0e73156a614ab2639c5f2fec4b863a
Marina_Koch <107465368+MarinaKoch1985@users.noreply.github.com>
2022-11-22 19:30:04 +0100

    add_user function added

--- teams_master/teams_master/src/components/App/App.jsx
+++ teams_master/teams_master/src/components/App/App.jsx
@@ -1,19 +1,38 @@
-import ConfiguratipnsPage from "../../pages/ConfigurationsPage";
-import TeamsPafe from "../../pages/TeamsPage";
+import ConfigurationsPage from "../../pages/ConfigurationsPage";
+import TeamsPage from "../../pages/TeamsPage";
 import Nav from "../Nav";
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import { Context } from '../../context'
 
 function App() {
+
+  const [ teams, setTeams ] = useState([]);
+  const [ users, setUsers ] = useState([]);
+
+  const addTeam = (team_title) => {
+    setTeams([...teams, { value: team_title, label: team_title }])
+  }
+
+  const addUser = (name_value, team_value) => {
+    setUsers([...users, {
+      id: Date.now(),
+      name: name_value,
+      team: team_value
+    }])
+  }
+
+  const deleteUser = () => {}
+  // Реализовать удаление карточки при клике на нее (метод filter)
+
   return (
-    <div >
+    <Context.Provider value={{ teams, addTeam, addUser, users }}>
       <Nav />
       <Routes>
-        
-        <Route path="/configurations_page" element={<ConfiguratipnsPage />} />
-        <Route path="/teams_page" element={<TeamsPafe />} />
-        
+        <Route path="/configurations_page" element={<ConfigurationsPage />} />
+        <Route path="/teams_page" element={<TeamsPage />} />
       </Routes>
-    </div>
+    </Context.Provider>
   );
 }
 
