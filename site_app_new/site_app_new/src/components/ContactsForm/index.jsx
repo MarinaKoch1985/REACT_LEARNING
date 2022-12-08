974a27baaf45188461c2d4e445dd27235757b7ee
Marina_Koch <107465368+MarinaKoch1985@users.noreply.github.com>
2022-12-08 01:28:18 +0100

    use_form added

--- site_app_new/site_app_new/src/components/ContactsForm/index.jsx
+++ site_app_new/site_app_new/src/components/ContactsForm/index.jsx
@@ -1,27 +1,73 @@
 import React from 'react'
 import s from './index.module.css'
+import {useForm} from 'react-hook-form'
 
 export default function ContactsForm() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    mode: 'onBlur'
+  });
+
+  const firstnameRegister = register ('firstname', {
+    required: '*Field "firstname" is required'
+    
+  });
+
+  const lastnameRegister = register ('lastname', {
+    required: '*Field "lastname" is required'
+    
+  });
+
+  const messageRegister = register('message', {
+    required: '*Field "message" is required'
+  });
+
+  const submit = (data) => console.log(data )
+
   return (
-    <form className={s.contacts_form}>
+    <div>
+    <form onSubmit={handleSubmit(submit)} className={s.contacts_form}>
       <div className={s.name}>
-      <label htmlFor="">
+
+      <label >
         <p>Имя</p>
-        <input type="text" name='firstname' placeholder='Иван' />
+        <input 
+        type="text" 
+        name='firstname' 
+        placeholder='Иван' 
+        {...firstnameRegister} />
       </label>
 
-      <label htmlFor="">
+      <label>
         <p>Фамилия</p>
-        <input type="text" name='lastname' placeholder='Иванов' />
+        <input 
+        type="text" 
+        name='lastname' 
+        placeholder='Иванов' 
+        {...lastnameRegister} />
       </label>
       </div>
       
 
-      <label htmlFor="">
+      <label>
         <p>Сообщение</p>
-        <textarea type="text" name='massege' placeholder='Ваше сообщение' />
+        <textarea t
+        ype="text" 
+        name='message' 
+        placeholder='Ваше сообщение'
+        {...messageRegister} />
       </label>
+
       <button>Отправить сообщение</button>
     </form>
+
+    <div>
+      {errors.firstname && <p>{errors.firstname?.message}</p>}
+      {errors.lastname && <p>{errors.lastname?.message}</p>}
+      {errors.message && <p>{errors.message?.message}</p>}
+    </div>
+
+    </div>
+    
   )
 }
