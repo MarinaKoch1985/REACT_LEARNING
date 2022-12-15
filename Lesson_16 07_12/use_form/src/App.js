6abbee269fa917fb81ff1f9a26351ea3e689a70e
Marina_Koch <107465368+MarinaKoch1985@users.noreply.github.com>
2022-12-15 19:27:58 +0100

    link to reset_password addet

--- Lesson_16 07_12/use_form/src/App.js
+++ Lesson_16 07_12/use_form/src/App.js
@@ -1,11 +1,25 @@
-import FormExample from "./components/FormExample";
+import { useState } from "react";
+import Button from "./components/Button";
+import Modal from "./components/Modal";
+import { Link } from 'react-router-dom'
 
 function App() {
+
+  const [ modal, setModal ] = useState(false);
+
   return (
-    <div >
-     <FormExample />
+    <div>
+      <Link to='/registration_form' style={{ textDecoration: 'none' }}>
+        <Button color='yellow' onClick={() => setModal(true)}>
+          Open modal window
+        </Button>
+      </Link>
+
+      <Modal modal={modal} setModal={setModal} />
+
     </div>
   );
 }
 
 export default App;
+    
