9034639bedc1fbb3367c69f7166813268c818c73
Marina_Koch <107465368+MarinaKoch1985@users.noreply.github.com>
2022-12-26 19:09:48 +0100

    layout added

--- REDUX Practuc/redux_practice/src/App.js
+++ REDUX Practuc/redux_practice/src/App.js
@@ -2,16 +2,17 @@ import MainPage from './pages/ManePage'
 import UsersPage from './pages/UsersPage'
 import PostsPage from './pages/PostsPage'
 import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div>
      < Routes >
-
-     <Route path='/' element={<MainPage />}/>
-     <Route path='/users' element={<UsersPage />}/>
-     <Route path='/posts' element={<PostsPage />}/>
-     
+        <Route path='/' element={<Layout />}>
+          <Route index element={<MainPage />}/>
+          <Route path='/users' element={<UsersPage />}/>
+          <Route path='/posts' element={<PostsPage />}/>
+        </Route>
      </Routes>
     </div>
   );
