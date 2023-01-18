cc945e9aac650413dbc4305d02a966b16bb80746
Marina_Koch <107465368+MarinaKoch1985@users.noreply.github.com>
2023-01-18 18:54:21 +0100

    product link added

--- Lesson 11_01 routing_practice/routing_practice/src/components/ProductCard/index.jsx
+++ Lesson 11_01 routing_practice/routing_practice/src/components/ProductCard/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import s from './index.module.css'
+import { Link } from 'react-router-dom'
 
-export default function ProductCard({ title, price, images }) {
+export default function ProductCard({ id, title, price, images }) {
+  
   return (
-    <div className={s.product_card}>
+    <Link className={s.product_card} to={`/product/${id}`}>
       <img src={images[0]} alt={title} />
       <p>{ title }</p>
       <p>Price: { price }$</p>
-    </div>
+    </Link>
   )
 }
