1568633364e809768912e011e0e2e3739129f02c
Marina_Koch <107465368+MarinaKoch1985@users.noreply.github.com>
2023-01-31 17:38:06 +0100

    add_product, delete card, clear_cart added

---
+++ Lesson 23_01 cart_master/cart_master/src/components/Cart/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import CartCard from '../CartCard';
+import { clearCart } from '../../store/reducers/cartReducer';
+
+export default function Cart() {
+
+  const cart = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+
+  const clear_cart = () => dispatch(clearCart());
+
+  const total = cart.reduce((prev, {price, count}) => prev + price * count, 0);
+
+
+  return (
+
+   <div>
+    <p>Cart</p>
+      <div>
+        {
+          cart.map(el => <CartCard key={el.id} {...el}/>)
+        }
+      </div>
+      <div>
+        <p>Total: { total } </p>
+        <button onClick={clear_cart}>Clear cart</button>
+      </div>
+    </div>
+  )
+}
