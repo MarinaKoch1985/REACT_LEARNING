551187fed67200772ef692c86ec27a19aa479c5a
Marina_Koch <107465368+MarinaKoch1985@users.noreply.github.com>
2023-02-28 16:16:16 +0100

    letzt

--- garden_online_shop/garden_online_shop/src/pages/ProductsSalePage/index.jsx
+++ garden_online_shop/garden_online_shop/src/pages/ProductsSalePage/index.jsx
@@ -1,43 +1,27 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
 import { useEffect} from 'react'
-import { load_products } from '../../requests/products_req'
+import { load_all_products } from '../../requests/allProducts_req'
 import ProductSaleCard from '../../components/ProductSaleCard'
 import { useSelector, useDispatch } from 'react-redux';
 import s from './index.module.css'
+import SortProducts from '../../components/SortProducts';
 
 export default function ProductsSalePage() {
 
-  // const { category } = useParams();
+   
 
     const dispatch = useDispatch();
     const products = useSelector(state => state.products);
 
       useEffect(() => {
-      dispatch(load_products)
+      dispatch(load_all_products)
       }, []) 
 
   return (
 
-    <div className={s.prod_cont}>
+    <div>
 
-       <div>
-          <span>Price:</span>
-          <form>
-            <input type="number" placeholder='from' name='min' />
-            <input type="number" placeholder='to' name='max' />
-            <button>Search</button>
-          </form>
-        </div>
-
-        <div>
-          <span>Sort by:</span>
-          <select name="" id="">
-            <option value="default">default</option>
-            <option value="title">title</option>
-            <option value="price">price</option>
-          </select>
-        </div>
+    <SortProducts/>
 
       <h1>Акции</h1>
 
